3001d0b84078a2ff34f53d6db7bdc2907f397758
vldyslavhrynchuk <vlad.hrynchuk@gmail.com>
2022-01-20 11:58:27 +0000

    modified folder_organizer.sh

--- Section_9/folder_organizer.sh
+++ Section_9/folder_organizer.sh
@@ -13,13 +13,13 @@ read -p "Please select a folder to organize: " folder
 while read file; do
         case "$file" in
         *.jpg | *.jpeg | *.png) mkdir -p "$folder"/images; mv "$file" "$folder"/images/;;
-        *.doc | *.docx | *.txt | *.pdf) mkdir -p "$folder"/idiocuments; mv "$file" "$folder"/idocuments/;;
-        *.xls | *.xlsx | *.csv) mkdir -p "$folder"/ispreadsheets; mv "$file" "$folder"/ispreadsheets/;;
-        *.sh) mkdir -p "$folder"/iscripts; mv "$file" "$folder"/iscripts/;;
-        *.zip | *.tar | *.tar.gz | *.tar.bz2) mkdir -p "$folder"/iarchives; mv "$file" "$folder"/iarchives/;;
-        *.ppt | *.pptx) mkdir -p "$folder"/ipresentations; mv "$file" "$folder"/ipresentations/;;
-        *.mp3) mkdir -p "$folder"/iaudio; mv "$file" "$folder"/iaudio/;;
-        *.mp4) mkdir -p "$folder"/ivideo; mv "$file" "$folder"/ivideo/;;
+        *.doc | *.docx | *.txt | *.pdf) mkdir -p "$folder"/documents; mv "$file" "$folder"/documents/;;
+        *.xls | *.xlsx | *.csv) mkdir -p "$folder"/spreadsheets; mv "$file" "$folder"/spreadsheets/;;
+        *.sh) mkdir -p "$folder"/scripts; mv "$file" "$folder"/scripts/;;
+        *.zip | *.tar | *.tar.gz | *.tar.bz2) mkdir -p "$folder"/archives; mv "$file" "$folder"/archives/;;
+        *.ppt | *.pptx) mkdir -p "$folder"/presentations; mv "$file" "$folder"/presentations/;;
+        *.mp3) mkdir -p "$folder"/audio; mv "$file" "$folder"/audio/;;
+        *.mp4) mkdir -p "$folder"/video; mv "$file" "$folder"/video/;;
         *) ;;
         esac
 
